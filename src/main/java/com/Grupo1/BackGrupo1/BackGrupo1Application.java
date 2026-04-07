52bad900a26ac0b8974edda91d06386bd53d5ccd
Pedro Guerra <pedrohsguerra1@gmail.com>
2026-04-07 19:55:29 -0300

    first commit

---
+++ src/main/java/com/Grupo1/BackGrupo1/BackGrupo1Application.java
@@ -0,0 +1,13 @@
+package com.Grupo1.BackGrupo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackGrupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackGrupo1Application.class, args);
+	}
+
+}
