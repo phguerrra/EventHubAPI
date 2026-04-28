161ff4f02a496e1f50962042411f24ba5b57be65
Pedro <pedro@example.com>
2026-04-27 21:22:35 -0300

    Teste Unitario adicionado

---
+++ src/main/java/com/grupo1/backGrupo1/exception/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.grupo1.backGrupo1.exception;
+
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
