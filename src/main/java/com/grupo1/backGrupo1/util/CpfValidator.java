585ba677c11dc8607155f69d58f9cb443ea1a15e
Augusto Souza <augusto281003@gmail.com>
2026-04-28 19:44:59 -0300

    correção da pasta util

--- src/main/java/com/grupo1/backGrupo1/util/CpfValidator.java
+++ src/main/java/com/grupo1/backGrupo1/util/CpfValidator.java
@@ -4,18 +4,14 @@ public class CpfValidator {
 
     public static boolean isValid(String cpf) {
 
-        // remove caracteres (., -)
         cpf = cpf.replaceAll("[^0-9]", "");
 
-        // verifica tamanho
         if (cpf.length() != 11) return false;
 
-        // evita sequências iguais (11111111111)
         if (cpf.matches("(\\d)\\1{10}")) return false;
 
         int soma = 0;
 
-        // cálculo do primeiro dígito
         for (int i = 0; i < 9; i++) {
             soma += (cpf.charAt(i) - '0') * (10 - i);
         }
@@ -23,7 +19,6 @@ public class CpfValidator {
         int digito1 = 11 - (soma % 11);
         if (digito1 >= 10) digito1 = 0;
 
-        // cálculo do segundo dígito
         soma = 0;
         for (int i = 0; i < 10; i++) {
             soma += (cpf.charAt(i) - '0') * (11 - i);
@@ -32,7 +27,6 @@ public class CpfValidator {
         int digito2 = 11 - (soma % 11);
         if (digito2 >= 10) digito2 = 0;
 
-        // valida final
         return digito1 == (cpf.charAt(9) - '0') &&
                 digito2 == (cpf.charAt(10) - '0');
     }
