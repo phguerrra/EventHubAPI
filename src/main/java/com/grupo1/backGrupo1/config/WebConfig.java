dca52b78f03fc6c9c0112ae4acf01c0cdbc31f0c
laurence <laurencemiguel1@gmail.com>
2026-06-01 22:40:53 -0300

    feat: enable event image upload

--- src/main/java/com/grupo1/backGrupo1/config/WebConfig.java
+++ src/main/java/com/grupo1/backGrupo1/config/WebConfig.java
@@ -4,6 +4,8 @@ import com.grupo1.backGrupo1.security.AdminInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import java.nio.file.Paths;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -17,5 +19,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(adminInterceptor);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String uploadPath = Paths.get("uploads").toAbsolutePath().toUri().toString();
+
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations(uploadPath);
     }
 }
